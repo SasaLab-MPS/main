08ac1d047d83606312d907c4b1fb1ef12bc96d39
SasaLab-MPS <4520510@ed.tus.ac.jp>
2021-02-21 20:17:35 +0900

    calGravity実装

--- MPS_SEC1/include/functions.hpp
+++ MPS_SEC1/include/functions.hpp
@@ -19,10 +19,10 @@ void structBkt(void);                                          // 解析領域�
 void mainLoopOfSimulation(void);                               // メインループ
 void writeData_inProfFormat(void);                             // 計算結果の出力(独自ファイル形式)
 void writeData_inVtuFormat(void);                              // 計算結果をファイル出力(ParaViewのvtuファイル形式)
-
-
-
 void calGravity(void);                                         // NS方程式右辺第三項を計算，重力による粒子の加速を計算
+
+
+
 void calViscosity(void);                                       // NS方程式右辺第二項を計算，粘性による粒子の加速を計算
 void moveParticle(void);                                       // 粒子の移動を計算
 void collision(void);                                          // 剛体衝突関数(粒子同士が異常接近した場合に粒子間の距離を広げる)
