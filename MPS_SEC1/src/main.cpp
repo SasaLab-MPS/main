3f3c9673b10f51e1e8064322027e7b724ad304ac
SasaLab-MPS <4520510@ed.tus.ac.jp>
2021-02-23 12:39:27 +0900

    main追加

---
+++ MPS_SEC1/src/main.cpp
@@ -0,0 +1,22 @@
+#include "../include/inputs.hpp"
+#include "../include/functions.hpp"
+
+// 計算領域の指定
+double x_width = 0.25, y_height = 0.5, z_depth = 0.1;
+
+int main(int argc, char **argv)
+{
+    printf("\n*** START PARTICLE-SIMULATION ***\n");
+    if (DIM == 2)
+    {
+        initializeParticlePositionAndVelocity_for2dim(x_width, y_height);
+    }
+    else
+    {
+        initializeParticlePositionAndVelocity_for3dim(x_width, y_height, z_depth);
+    }
+    calConstantParameter();
+    mainLoopOfSimulation();
+    printf("*** END ***\n\n");
+    return 0;
+}
