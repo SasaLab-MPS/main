d1a76381faf58cb28b22199bfb399269ea319cf6
SasaLab-MPS <4520510@ed.tus.ac.jp>
2021-04-29 16:00:38 +0900

    修正8

--- HeatTransfer/include/inputs.hpp
+++ HeatTransfer/include/inputs.hpp
@@ -18,7 +18,7 @@ using namespace Eigen;
 constexpr int DIM = 2;                      // 次元
 constexpr double PARTICLE_DISTANCE = 0.04;  // 初期粒子間距離 l0 (mm)
 constexpr double DT = 0.001;                // 時間刻み幅
-constexpr int OUTPUT_INTERVAL = 1;          // 計算結果のファイル出力の間隔
+constexpr int OUTPUT_INTERVAL = 20;         // 計算結果のファイル出力の間隔
 
 /* for three-dimensional simulation */
 /*
@@ -28,7 +28,7 @@ constexpr double DT = 0.003;
 constexpr int OUTPUT_INTERVAL = 20;
 */
 
-constexpr double FINISH_TIME = 0.01;                      // シミュレーションの終了時刻
+constexpr double FINISH_TIME = 2;                         // シミュレーションの終了時刻
 constexpr double KINEMATIC_VISCOSITY = 1.0e-6;            // 動粘性係数
 constexpr double FLUID_DENSITY = 1000.0;                  // 流体の密度
 constexpr double G_X = 0.0;
@@ -62,7 +62,7 @@ constexpr int DIRICHLET_BOUNDARY_IS_CHECKED = 2;            // ディリクレ�
 constexpr double CRT_NUM = 0.1;                             // クーラン数
 
 constexpr double INITIAL_TEMPERATURE = 0.0;                 // 初期温度 (℃)
-constexpr double SOLID_DENSITY = 8e-3;                      // 固体の密度:ρ (g/mm^3)
+constexpr double SOLID_DENSITY = 8000;                      // 固体の密度:ρ (g/mm^3)
 constexpr double SPECIFIC_HEAT_CAPACITY = 500;              // 比熱容量:c (J/gK)
 constexpr double HEAT_CONDUCTIVITY = 300;                   // 熱伝導率:λ (J/mmKs)
 
