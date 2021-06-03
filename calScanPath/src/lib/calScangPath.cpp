c4a4daf6399f169f63ed59385ae2d03894aa7507
SasaLab-MPS <4520510@ed.tus.ac.jp>
2021-06-03 21:47:09 +0900

    スキャンパス実装開始

--- calScanPath/src/lib/calScangPath.cpp
+++ calScanPath/src/lib/calScangPath.cpp
@@ -10,23 +10,45 @@
 void calScangPath(string strategy, double length) {
     /* レーザの中心座標を返すようにした方が良いか? */
     double p = DT * LASER_POWER;                    // レーザ出力
-    double travelDistance = Time * SCAN_SPEED;      // 合計の走査距離
-    int repetitionNum = travelDistance / length;    // レーザが折り返した回数
-    double h = SCAN_PITCH;
     Coordinate centerOfLaser;                       // レーザの中心座標
+
+    double travelDistance = Time * SCAN_SPEED;      // 合計の走査距離
+    double h = SCAN_PITCH;
+
+    int totalRepetition = travelDistance / length;  // レーザが折り返した総数
+    
+    int nx;     // x 方向に繰り返せる最大数
+    int ny;     // y 方向に繰り返せる最大数
+
+    int k, l, m;
+
+    double y = (double)m * h;
 
     if (strategy == "Stripe")
     {
         /* code */
     }
     else if (strategy == "Island") {
-        int n = y_MAX / h;           // y方向に繰り返せる最大数
-        int m = repetitionNum % n; 
-        double y = (double)m * h;
+        ny = y_MAX / h;
+        m = totalRepetition / ny;       // 短冊の総数
+        k = totalRepetition - m * ny;   // そのストライプの中でレーザが行き来した回数
         
-        // レーザの中心座標の計算
-        centerOfLaser.x = travelDistance - (double)(repetitionNum / n)* length;
-        centerOfLaser.y = y;
+        /* レーザの中心座標の計算 */
+        double tx = travelDistance - (double)totalRepetition * length;
+        double ty = (double)k * h;
+        // x 座標の計算
+        if (k % 2 == 1) {
+            centerOfLaser.x = (double)(m+1) * length - tx;
+        } else {
+            centerOfLaser.x = (double)m * length + tx;
+        }
+        // y 座標の計算
+        if (tx > 0 && tx < y_MAX) {
+            centerOfLaser.y = ty + h;
+        } else {
+            centerOfLaser.y = ty;
+        }
+        
         centerOfLaser.z = z_MAX;
     }
     else {
