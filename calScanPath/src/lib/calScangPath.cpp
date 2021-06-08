69d5a219c9ae02fb5dede099928929a6ebf0a113
SasaLab-MPS <4520510@ed.tus.ac.jp>
2021-06-08 12:44:45 +0900

    アイランド走査経路実装完了?

--- calScanPath/src/lib/calScangPath.cpp
+++ calScanPath/src/lib/calScangPath.cpp
@@ -13,29 +13,84 @@ Coordinate calScangPath(string strategy, double length)
     Coordinate centerOfLaser;                       // レーザの中心座標
 
     double travelDistance = Time * SCAN_SPEED;      // 合計の走査距離
-    double h = SCAN_PITCH;
-
+    double hatch = SCAN_PITCH;
     int totalRepetition = travelDistance / length;  // レーザが折り返した総数
     
-    int nx;     // x 方向に繰り返せる最大数
-    int ny;     // y 方向に繰り返せる最大数
 
-    int k, l, m;
-
-    double y = (double)m * h;
-
-    if (strategy == "Stripe")
+    if (strategy == "Island")
     {
-        /* code */
+        int numOfLine = length/hatch;       // アイランド内のライン本数
+        int Lx = x_MAX / length, Ly = y_MAX / length;   // Lx: x方向一列のブロック数, Ly: y方向一列のブロック数
+        //int nX = Ix, nY = Iy * nX;
+        int scanBlocks = totalRepetition / numOfLine;   // 走査済みのアイランド数
+        int low = scanBlocks / Lx;          // 走査済みの列数
+        int n = scanBlocks - low * Lx;      // その列での走査済みのブロック数
+
+        double x, y;    // 基準座標，そのアイランドの左下を基準座標にする
+
+        x = (double)n * length;
+        y = (double)low * length;
+
+        if (n%2 == 0)   
+        {
+            /* 偶数ならx方向 */
+            // そのアイランドでの走査済み本数
+            int ix = totalRepetition - scanBlocks * numOfLine;
+            double lx;      // x = x + lx みたいにしたい
+            lx = travelDistance - (double)totalRepetition * length;
+
+            if (ix%2 == 0)
+            {
+                /* 偶数なら左方向に走査 */
+                x = x + lx;
+            }
+            else
+            {
+                /* 奇数なら右方向に走査 */
+                x = x + length - lx;
+            }
+            y = y + ix * hatch;
+            
+        }
+        else
+        {
+            /* 奇数ならy方向 */
+            int iy = totalRepetition - scanBlocks * numOfLine;
+            double ly; 
+            ly = travelDistance - (double)totalRepetition * length;
+
+            if (iy % 2 == 0)
+            {
+                /* 偶数なら上方向に走査 */
+                y = y + ly;
+            }
+            else
+            {
+                /* 奇数なら下方向に走査 */
+                y = y + length - ly;
+            }
+            x = x + iy * hatch;
+        }
+
+        centerOfLaser.x = x;
+        centerOfLaser.y = y;
+    
     }
-    else if (strategy == "Island") {
-        ny = y_MAX / h;
+    else if (strategy == "Stripe") {
+        int nx; // x 方向に繰り返せる最大数
+        int ny; // y 方向に繰り返せる最大数
+
+        int k, l, m;
+        double X = x_MAX, Y = y_MAX;
+        double y = (double)m * hatch;
+
+        ny = Y / hatch;
         m = totalRepetition / ny;       // 短冊の総数
         k = totalRepetition - m * ny;   // そのストライプの中でレーザが行き来した回数
         
         /* レーザの中心座標の計算 */
         double tx = travelDistance - (double)totalRepetition * length;
-        double ty = (double)k * h;
+        double ty = (double)k * hatch;
         // x 座標の計算
         if (k % 2 == 1) {
             centerOfLaser.x = (double)(m+1) * length - tx;
@@ -43,8 +98,8 @@ Coordinate calScangPath(string strategy, double length)
             centerOfLaser.x = (double)m * length + tx;
         }
         // y 座標の計算
-        if (tx > 0 && tx < y_MAX) {
-            centerOfLaser.y = ty + h;
+        if (tx > 0 && tx < Y) {
+            centerOfLaser.y = ty + hatch;
         } else {
             centerOfLaser.y = ty;
         }
