8d3501a4c7a79783b8325215cde5138826168163
SasaLab-MPS <4520510@ed.tus.ac.jp>
2021-06-21 21:42:20 +0900

    StripeStrategy修正

--- calScanStrategy/src/lib/stripePattern.cpp
+++ calScanStrategy/src/lib/stripePattern.cpp
@@ -9,40 +9,103 @@
 #include "../../include/inputs.hpp"
 
 void stripePattern(void) {
+    Coordinate P1, P2, Ref; // 移動前のレーザ中心:P1，移動後:P2，そのストラテジーの照射開始点:Ref
+    P1.x = centerOfLaser.x;
+    P1.y = centerOfLaser.y;
+    Ref.x = referencePoint.x;
+    Ref.y = referencePoint.y;
 
-    double travelDistance = Time * SCAN_SPEED; // 合計の走査距離
-    double hatch = SCAN_PITCH;
-    double scanVectorLength = SCAN_VECTOR_LENGTH;
-    int totalRepetition = travelDistance / scanVectorLength; // レーザが折り返した総数
+    double hatch, scanSpeed, scanVectorLength;
+    hatch = SCAN_PITCH;
+    scanSpeed = SCAN_SPEED;
+    scanVectorLength = SCAN_VECTOR_LENGTH;
 
-    int ny; // y 方向に繰り返せる最大数
+    // 照射中心の更新
+    double edgeOfX, edgeOfY;               // そのストラテジーで照射できるxとy方向の端
+   
+    /* 走査経路の計算 */
+    edgeOfX = x_MAX;
 
-    int k, l;
-    double Y = y_MAX;
-
-    ny = Y / hatch;
-    l = totalRepetition / ny;     // 短冊の総数
-    k = totalRepetition - l * ny; // そのストライプの中でレーザが行き来した回数
-
-    /* レーザの中心座標の計算 */
-    double tx = travelDistance - (double)totalRepetition * scanVectorLength;
-    double ty = (double)k * hatch;
-    // x 座標の計算
-    if (k % 2 == 1)
+    if (ScanDirection == FORWARD)
     {
-        centerOfLaser.x = (double)(l + 1) * scanVectorLength - tx;
+        P2.x = P1.x;
+        P2.y = P1.y + scanSpeed * DT;
+
+        edgeOfY = NumOfStrategy * scanVectorLength + scanVectorLength;
+        
+        // yの端に到達したとき
+        if ((edgeOfY + EPS) < P2.y || (y_MAX + EPS) < P2.y) {
+            P2.x = P1.x + hatch;
+            P2.y = P1.y;
+            ScanDirection = REVERSE;
+        }
+
+        // xの端に到達したとき
+        if ((edgeOfX + EPS) < P2.x) {
+            NumOfStrategy++; // ストライプ番号の更新
+            P2.x = Ref.x;
+            if (NumOfStrategy % 2 == 1) {
+                ScanDirection = REVERSE;
+                P2.y = edgeOfY + scanVectorLength;
+            } else {
+                ScanDirection = FORWARD;
+                P2.y = edgeOfY;
+            }
+            
+            if (P2.y > y_MAX) {
+                P2.y = y_MAX;
+            }           
+            
+            // 後処理
+            referencePoint.x = P2.x;
+            referencePoint.y = P2.y;
+        }
+
     }
     else
     {
-        centerOfLaser.x = (double)l * scanVectorLength + tx;
+        P2.x = P1.x;
+        P2.y = P1.y - scanSpeed * DT;
+
+        edgeOfY = NumOfStrategy * scanVectorLength;
+
+        // yの端に到達したとき
+        if (P2.y < (edgeOfY - EPS) || P2.y < (Pos_MIN[1] - EPS)) {
+            P2.x = P1.x + hatch;
+            P2.y = Ref.y;
+            ScanDirection = FORWARD;
+        }
+
+        // xの端に到達したとき
+        if ((edgeOfX + EPS) < P2.x) {
+            NumOfStrategy++; // ストライプ番号の更新
+            P2.x = Ref.x;
+            if (NumOfStrategy % 2 == 1) {
+                ScanDirection = REVERSE;
+                edgeOfY = edgeOfY + 2 * scanVectorLength;
+            } else {
+                ScanDirection = FORWARD;
+                edgeOfY = edgeOfY + scanVectorLength;
+            }
+
+            if (P2.y > y_MAX) {
+                P2.y = y_MAX;
+            }
+
+            // 後処理         
+            referencePoint.x = P2.x;
+            referencePoint.y = P2.y;
+        }
     }
-    // y 座標の計算
-    if (tx > 0 && tx < Y)
-    {
-        centerOfLaser.y = ty + hatch;
+    
+   
+    // 照射中心の更新
+    centerOfLaser.x = P2.x;
+    centerOfLaser.y = P2.y;
+    if (DIM == 2) {
+        centerOfLaser.z = 0.0;
+    } else {
+        centerOfLaser.z = z_MAX;
     }
-    else
-    {
-        centerOfLaser.y = ty;
-    }
+    
 }
