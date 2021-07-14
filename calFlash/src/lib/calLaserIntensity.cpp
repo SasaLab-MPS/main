d7eb8f8e6525926b8805174d2b2d664e9c39972f
SasaLab-MPS <4520510@ed.tus.ac.jp>
2021-07-14 11:59:09 +0900

    瞬間熱源

---
+++ calFlash/src/lib/calLaserIntensity.cpp
@@ -0,0 +1,37 @@
+/*=====================================================================
+  calLaserRadiation.cpp   
+  Yota INOUE (2021) 
+  走査パターンを計算
+  Last update: June 3, 2021
+=======================================================================*/
+#include "../../include/functions.hpp"
+#include "../../include/inputs.hpp"
+
+double calLaserIntensity(Particle P)
+{
+    double power = LASER_ABSORPTION_RATE * DT * LASER_POWER; // レーザ出力，WからJに変換，吸収率を掛ける
+    double intensity = 0.0;
+    double a = 0.5 * LASER_DIAMETER;    // レーザ半径，ガウス分布のσに相当
+
+    // 強度計算
+    double rx, ry;  // レーザ中心からの距離
+    double r, r2;   // 中間変数
+    rx = P.x - centerOfLaser.x;
+    ry = P.y - centerOfLaser.y;
+    r2 = rx * rx + ry * ry;
+    r = sqrt(r2);
+    if (r < 3.1 * a) {
+        // レーザ強度の計算
+        intensity = (power / (M_PI * a * a)) * exp((-1) * r2 / (a * a));
+        // 面積当たりの投入エネルギーに変換
+        intensity *= (PARTICLE_DISTANCE * PARTICLE_DISTANCE);
+    }
+
+    if (DIM == 2) {
+        P.z = 0.0;
+    } else {
+        P.z = z_MAX;
+    }
+    
+    return intensity;
+}
---
+++ calFlash/src/lib/searchBucket.cpp
@@ -0,0 +1,44 @@
+/*=====================================================================
+  searchBkt.cpp   
+  Yota INOUE (2021) 
+  関数実装ファイル 
+  NS方程式右辺第二項を計算
+  Last update: Feb 21, 2021
+=======================================================================*/
+#include "../../include/functions.hpp"
+#include "../../include/inputs.hpp"
+
+void searchBucket(int i) {
+  int particleID;
+  int id;   // バケットのid
+  neghPar.clear(); // 要素の全削除
+  /* バケット法による粒子の探索効率向上 */
+  // 粒子iの所属するバケットのid
+  int bktid[3];   // 0:x, 1:y, 2:z
+  double MIN[3];  // 領域の最小値
+  for (int j = 0; j < 3; j++) {
+    MIN[j] = Pos_MIN[j] - PARTICLE_DISTANCE * 4;
+  }
+
+  bktid[0] = (int)((particle[i].x - MIN[0]) * DBinv) + 1;
+  bktid[1] = (int)((particle[i].y - MIN[1]) * DBinv) + 1;
+  bktid[2] = (int)((particle[i].z - MIN[2]) * DBinv) + 1;
+
+  // 対象粒子i周辺の粒子をバケット法により探索 3*3*3=27
+  for (int jz = bktid[2] - 1; jz <= bktid[2] + 1; jz++) {
+    for (int jy = bktid[1] - 1; jy <= bktid[1] + 1; jy++) {
+      for (int jx = bktid[0] - 1; jx <= bktid[0] + 1; jx++) {
+        // バケットのidを計算, 2次元のときnBxy = 0としている
+        id = jz * nBxy + jy * nBx + jx;
+        for (int j = 0; j < (int)bucket[id].size(); j++) {
+          particleID = bucket[id][j];
+          neghPar.emplace_back(particleID);
+        }
+      }
+    }
+  }
+
+  // 重複を削除
+  sort(neghPar.begin(), neghPar.end());
+  neghPar.erase(unique(neghPar.begin(), neghPar.end()), neghPar.end());
+}
